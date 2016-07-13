e87915591db6120109b7338125a531cbb2c43630
Sucreyy1 <593143487@qq.com>
2016-07-13 19:08:56 +0800

    timer

--- index/js.js
+++ index/js.js
@@ -5,10 +5,62 @@ $(function(){
 	$('.search_btn').hover(function(){
 		$(this).addClass('search_btn:hover');
 	});
+	/*小圆点控制*/
 	$('.img li').eq(0).show();
 	$('.num li').bind('click',function(){
 		$(this).addClass('selected').siblings().removeClass('selected');
 		var i=$(this).index();
-		$('.img li').eq(i).fadeIn().siblings().fadeOut();
+		$('.img li').eq(i).stop().fadeIn().siblings().stop().fadeOut();
 	});
-})
+	/*箭头控制*/
+	$('.left').bind('click',function(){
+		var i=$('.num li.selected').index();
+		i=i-1;
+		if(i<0){
+			i=3;
+		}
+		$('.img li').eq(i).stop().fadeIn().siblings().stop().fadeOut();
+		$('.num li').eq(i).addClass('selected').siblings().removeClass('selected');
+	});
+		$('.right').bind('click',function(){
+		var i=$('.num li.selected').index();
+		i=i+1;
+		if(i>3){
+			i=0;
+		}
+		$('.img li').eq(i).stop().fadeIn().siblings().stop().fadeOut();
+		$('.num li').eq(i).addClass('selected').siblings().removeClass('selected');
+	});
+});
+	/*封装函数*/
+	function move(){
+		$('.right').click();
+		var i=$('.num li.selected').index();
+		if(i>3){
+			i=0;
+		}
+		$('.img li').eq(i).stop().fadeIn().siblings().stop().fadeOut();
+		$('.num li').eq(i).addClass('selected').siblings().removeClass('selected');
+	}
+	/*自动轮播*/
+	var timer=setInterval(move,3000);
+	/*清除定时器*/
+	$('.img li').hover(function(){
+		clearInterval(timer);
+		},function(){
+		timer=setInterval(move,3000);
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
